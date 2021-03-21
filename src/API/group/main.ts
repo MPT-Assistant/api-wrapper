8862bbbf9e1a9a1382c6d6b36ea18e365bab300e
RusAnonym <alexandrsemin2033@gmail.com>
2021-03-21 03:37:13 +0300

    add group.getList

--- src/API/group/main.ts
+++ src/API/group/main.ts
@@ -1,15 +1,20 @@
 import { API_Response, IOptions } from "../../types";
 
 import * as get from "./methods/get";
+import * as getList from "./methods/getList";
 
 import Builder from "../../lib/builder";
 
-export default class Schedule extends Builder {
+export default class Group extends Builder {
 	constructor(options?: IOptions) {
 		super(options);
 	}
 
 	public async get(params: get.Params): API_Response<get.Response> {
 		return this.postRequest(get.method, params);
+	}
+
+	public async getList(params: getList.Params): API_Response<getList.Response> {
+		return this.postRequest(getList.method, params);
 	}
 }
